5edefa18f769bca1377d03c3f42121fbcde4e90e
engektiar <eng.ektiar@gmail.com>
2024-01-01 00:43:43 +0600

    heppy new year row na column

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
- import 'package:flutter/material.dart';
 
-void main(){
+import 'package:flutter/material.dart';
+
+void main (){
   runApp(const MyApp());
  }
  class MyApp extends StatelessWidget {
@@ -8,31 +9,46 @@ void main(){
 
    @override
    Widget build(BuildContext context) {
-     return MaterialApp(
+     return  MaterialApp(
        debugShowCheckedModeBanner: false,
        home: SafeArea(
-         child: Scaffold(
-         appBar: AppBar(
-           title: Text("Home"),
-           centerTitle:true ,
-           leading: Icon(Icons.message),
-           actions: [
-             IconButton(
-               icon: Icon(Icons.add_a_photo),
-               onPressed: (){
+          child:Scaffold(
+            appBar: AppBar(
+              title: Text(" Home Page"),
+              centerTitle: true,
+              leading: Icon(Icons.message),
+              actions: [
+                IconButton(
+                  icon:Icon(Icons.add_a_photo),
+                  onPressed: (){},
+                ),
+              ],
+            ),
 
-               },
-             )
-           ],
+            body:Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                ),
+                SizedBox(width: 10,),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                SizedBox(width: 10,),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                )
+              ],
+            ),
 
-         ),
-        body: Container(
-          width: 300,
-          height: 400,
-            color: Colors.blue,
-        ),
+          ),
        ),
-     ),
      );
    }
  }
