fbdb43bc547b5ffea31c8623df72f1672a7aa78b
engektiar <eng.ektiar@gmail.com>
2023-12-31 21:44:40 +0600

    futter icon add way

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,30 @@
- import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 
 void main(){
   runApp(const MyApp());
  }
  class MyApp extends StatelessWidget {
    const MyApp({super.key});
- 
+
    @override
    Widget build(BuildContext context) {
      return MaterialApp(
        debugShowCheckedModeBanner: false,
        home: Scaffold(
-         body: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-             Text("Hi  This is Fullter developer thank you ")
+         appBar: AppBar(
+           title: Text("Home"),
+           centerTitle:true ,
+           actions: [
+             IconButton(
+               icon: Icon(Icons.add_a_photo),
+               onPressed: (){
+
+               },
+             )
            ],
+
          ),
        ),
      );
    }
  }
- 
