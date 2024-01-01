89fc6056b642103418ca9d0f26555cbc1c29cb18
engektiar <eng.ektiar@gmail.com>
2024-01-01 21:25:07 +0600

    row scrol added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-
+ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main (){
+void main(){
   runApp(const MyApp());
  }
  class MyApp extends StatelessWidget {
@@ -9,45 +9,36 @@ void main (){
 
    @override
    Widget build(BuildContext context) {
-     return  MaterialApp(
+     return MaterialApp(
        debugShowCheckedModeBanner: false,
        home: SafeArea(
-          child:Scaffold(
-            appBar: AppBar(
-              title: Text(" Home Page"),
-              centerTitle: true,
-              leading: Icon(Icons.message),
-              actions: [
-                IconButton(
-                  icon:Icon(Icons.add_a_photo),
-                  onPressed: (){},
-                ),
-              ],
-            ),
-
-            body:Row(
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.amber,
-                ),
-                SizedBox(width: 10,),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                ),
-                SizedBox(width: 10,),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.green,
-                )
-              ],
-            ),
-
-          ),
+         child: Scaffold(
+           appBar: AppBar(title: Text("Home Page"),),
+           body: SingleChildScrollView(
+             scrollDirection: Axis.horizontal,
+             child: Row(
+               children: [
+                 Container(
+                   width: 200,
+                   height: 200,
+                   color: Colors.green,
+                 ),
+                 SizedBox(width: 10,),
+                 Container(
+                   width: 200,
+                   height: 200,
+                   color: Colors.cyan,
+                 ),
+                 SizedBox(width: 10,),
+                 Container(
+                   width: 200,
+                   height: 200,
+                   color: Colors.green,
+                 )
+               ],
+             ),
+           ),
+         ),
        ),
      );
    }
