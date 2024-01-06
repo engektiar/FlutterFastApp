c8835a3048795307e35356b5987f62c630cd29b8
engektiar <eng.ektiar@gmail.com>
2024-01-06 23:39:13 +0600

    state full weget add kora hoisa

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,38 @@
- import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
+
+import 'page1.dart';
+import 'page2.dart';
+import 'page3.dart';
 
 void main(){
   runApp(const MyApp());
- }
- class MyApp extends StatelessWidget {
-   const MyApp({super.key});
+  }
+  class MyApp extends StatefulWidget {
+    const MyApp({super.key});
 
-   @override
-   Widget build(BuildContext context) {
-     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-       home: Scaffold(
-         appBar: AppBar(
-           title: Text("Hi Ektiar this is home page"),
-         ),
-         body: Column(
-           children: [
-             Expanded(
-               flex: 1,
-               child: Container(color: Colors.yellow,),
-             ),
-             Expanded(
-               flex: 1,
-               child: Container(color: Colors.red,),
-             ),
-             Expanded(
-               flex: 2,
-               child: Container(color: Colors.green,),
-             ),
-           ],
-         ),
-       ),
-     );
-   }
- }
+    @override
+    State<MyApp> createState() => _MyAppState();
+  }
+
+  class _MyAppState extends State<MyApp> {
+  PageController _controller=PageController(
+    initialPage: 0,
+  );
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Scaffold(
+          body: PageView(
+            controller: _controller,
+            children: [
+              page1(),
+              page2(),
+              page3(),
+            ],
+          ),
+        ),
+
+      );
+    }
+  }
---
+++ lib/page3.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class page3 extends StatelessWidget {
+  const page3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+    );
+  }
+}
